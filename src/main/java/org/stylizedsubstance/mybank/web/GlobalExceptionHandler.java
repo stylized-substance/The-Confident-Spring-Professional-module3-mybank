6fe02a199cedb68d16e488bb2c60ed360652331c
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2026-02-06 11:24:24 +0200

    refactored exception handler to return custom objects

--- src/main/java/org/stylizedsubstance/mybank/web/GlobalExceptionHandler.java
+++ src/main/java/org/stylizedsubstance/mybank/web/GlobalExceptionHandler.java
@@ -6,14 +6,25 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.stylizedsubstance.mybank.dto.ErrorDto;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     // Handles method argument validation errors that are annotated with @Valid
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
-        return new ErrorResponse(exception.getStatusCode().value(), exception.getDetailMessageArguments()[1].toString());
+
+    public ErrorDto handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        Integer statusCode = exception.getBody().getStatus();
+        List<ErrorDto.ErrorField> errorFields = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> new ErrorDto.ErrorField(fieldError.getField(), fieldError.getDefaultMessage()))
+                .toList();
+
+        return new ErrorDto(statusCode, errorFields);
     }
 
     // Handles request parameter errors
